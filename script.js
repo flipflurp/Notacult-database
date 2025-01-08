c9caaf63888dd7c31213dbc094c09302fa7a9a89
flipflurp <126386688+flipflurp@users.noreply.github.com>
2025-01-08 09:01:22 +0000

    added the proper images

--- script.js
+++ script.js
@@ -19,10 +19,16 @@ $('#login').submit(function(){
 $('#searchbar').submit(function(){
   enteredCode = $('#code').val().toLowerCase();
   if(enteredCode == 'balls'){
-    $('#result').attr('src','Images/Puppy.jpg');
+    $('#result').attr('src','Images/Convo1.jpeg');
   }
   else if(enteredCode == 'meow'){
-    $('#result').attr('src','Images/hugh jackman (1).PNG');
+    $('#result').attr('src','Images/Convo2.jpeg');
+  }
+  else if(enteredCode == 'chocolate'){
+    $('#result').attr('src','Images/Convo3.jpeg')
+  }
+  else if((enteredCode).is(':contains("gnieb")')){
+    $('#result').attr('src','Images/RedactedImage.png')
   }
   else{
     $('result').attr('alt','No result found');
