74aa76c6471046244b48909328acb4f7ccbef876
flipflurp <126386688+flipflurp@users.noreply.github.com>
2025-01-08 07:33:02 +0000

    accesss to folder

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var enteredCode ='';
 $('#queryBtn').click(function(){
   enteredCode = $('#code').val().toLowerCase();
   if(enteredCode == 'balls'){
-    $('#result').attr('src','Puppy.jpg');
+    $('#result').attr('src','Images/Puppy.jpg');
   }
   else if(enteredCode == 'meow'){
-    $('#result').attr('src','hugh jackman (1).PNG');
+    $('#result').attr('src','Images/hugh jackman (1).PNG');
   }
   else{
     $('result').attr('alt','No result found');
