908776d8dcfe9021c6dd9f45984bb460e3d0d67b
flipflurp <126386688+flipflurp@users.noreply.github.com>
2025-01-13 07:22:36 +0000

    repl update

--- script.js
+++ script.js
@@ -1,40 +1,80 @@
 var enteredCode ='';
 
+if(localStorage.getItem('loggedin') == 'true'){
+  $('#logindiv').hide();
+  $('#searchbardiv').show();
+  $('#logoutbtn').show();
+  $('#greetingname').text(localStorage.getItem('username'));
+}
+var users = {};
+users['Maryland'] = 'bcghjmpt';
+users['ADCarrie'] = 'bcghjmpt';
+users['Jerry'] = 'bcghjmpt';
+users['HarryCrotchGoblin'] = 'bcghjmpt';
+users["BarryDepressed"] = 'bcghjmpt';
+users['PerryThePlatypus'] = 'bcghjmpt';
+users['Gary'] = 'bcghjmpt';
+users['Terryble'] = 'bcghjmpt';
+
+
+
+
+
+
+
+
 $('#login').submit(function(){
-  
-  if($('#password').val() =='bcghjmpt'){
-    $('#login').hide();
-    $('#searchbar').show();
-    $('#notif').text('');
+  if($('#username').val() in users){
+    if(users[$('#username').val()] == $('#password').val()){
+      localStorage.setItem('loggedin', 'true');
+      localStorage.setItem('username', $('#username').val());
+      location.reload();
+    }
+    else{
+      $('#username').css('background-color','rgb(70, 70, 145)')
+      $('#password').attr('placeholder','');
+      $('#password').css('background-color','rgb(220,100,130)');
+      $('#password').val('');
+    }
   }
   else{
-    $('#notif').text('Password is incorrect');
-
+    $('#username').attr('placeholder','');
+    $('#username').css('background-color','rgb(220,100,130)');
+    $('#username').val('');
+    $('#password').val('');
+    
   }
-  $('#password').val('');
+  
+  
   return false;
 });
+
+$('#logoutbtn').click(function(){
+  localStorage.setItem('loggedin', 'false');
+  location.reload();
+});
+
 
 
 $('#searchbar').submit(function(){
   enteredCode = $('#code').val().toLowerCase();
   $('#result').show();
-  $('#notif').text('');
+  $('#resultnotif').hide('');
   if(enteredCode == 'balls'){
-    $('#result').attr('src','Images/Convo1.jpeg');
+    $('#result').attr('src','Convo1.jpeg');
   }
   else if(enteredCode == 'meow'){
-    $('#result').attr('src','Images/Convo2.jpeg');
+    $('#result').attr('src','Convo2.jpeg');
   }
   else if(enteredCode == 'chocolate'){
-    $('#result').attr('src','Images/Convo3.jpeg');
+    $('#result').attr('src','Convo3.jpeg');
   }
   else if(enteredCode == 'gnieb hc tirdle'){
-    $('#result').attr('src','Images/RedactedImage.png');
+    $('#result').attr('src','/RedactedImage.png');
   }
   else{
     $('#result').hide();
-    $('#notif').text('No result found');
+    $('#resultnotif').show();
     $('#code').val('');
     return false;
   }
